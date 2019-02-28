a5a9a240f64aaf5eb5db8ee4de18123d074ecbbc
Josh Matos <jmatos.5@aol.com>
2019-02-28 16:06:22 -0500

    nav bar logo created and added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,25 +4,37 @@ const navi = () => {
 	return (
 		<div className="navi">
 			<div class="nav-wrapper">
+				<div>
+					<img class="headshot" src="/images/sanJoshLogoTransperant.png" alt=""/>
+				</div>
+
 				<div class="block">
 					<button class="block home-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/07-512.png" alt=""/></button>
 				</div>
+
 				<div class="block about">
-				<button class="block home-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/aboutIcon.png" alt=""/></button>
-					{/* <h1>P</h1> */}
+				<button class="block about-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/aboutIcon.png" alt=""/></button>
 				</div>
+
 				<div class="block skills">
-				<button class="block home-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/skillsIcon.png" alt=""/></button>
-					{/* <h1>A</h1> */}
+				<button class="block skills-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/skillsIcon.png" alt=""/></button>
 				</div>
+
 				<div class="block myWork">
-				<button class="block home-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/myWorkIcon.png" alt=""/></button>
-					{/* <h1>C</h1> */}
+				<button class="block myWork-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/myWorkIcon.png" alt=""/></button>
 				</div>
+
 				<div class="block contact">
-				<button class="block home-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/whiteMailIcon.png" alt=""/></button>
-					{/* <h1>C</h1> */}
+				<button class="block contact-button" type="button" data-hover="CLICK ME" data-active="I'M ACTIVE"><img class="home-pic" src="/images/whiteMailIcon.png" alt=""/></button>
 				</div>
+
+				<div class="socialMediaIconsDiv">
+					<a href="https://www.instagram.com/sanjosh24/"><img class="socialMediaIcons" src="/images/instagramIcon.png" alt="instagram"/></a>
+					<a href="https://www.facebook.com/josh.matos.167"><img class="socialMediaIcons" src="/images/facebookIcon.png" alt="facebook"/></a>
+					<a href="https://www.linkedin.com/in/joshua-matos1/"><img class="socialMediaIcons" src="/images/linkedInIcon.png" alt="linkedin"/></a>
+					<a href="https://github.com/SanJosh24"><img class="gitHubIcons" src="/images/githubIcon.png" alt="github"/></a>
+				</div>
+
 			</div>
 		</div>
 	)
