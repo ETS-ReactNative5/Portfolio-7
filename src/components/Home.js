6d343c08dd19bf0faa92c630d69ad93b1b5e0d39
Josh Matos <jmatos.5@aol.com>
2019-03-07 23:57:08 -0500

    animations added to home page and positioned.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,128 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import { Link } from 'react-router-dom';
 import  Navi from './Nav';
 import '../App.css';
-// import sample from './sample.mp4';
+import '../typeText';
+import $ from "jquery";
+import anime from '../../node_modules/animejs/lib/anime.es.js';
+import { enable, destory } from 'splash-screen';
 
 class home extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  
+
+  componentDidMount () {
+    $('.ml12').each(function(){
+      $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+
+  // -=-=-=-=-=-=-=- This function appears and disappeared texted content. -=-=-=-=-=-=-=-
+    anime.timeline({loop: true})
+      .add({
+        targets: '.ml12 .letter',
+        translateX: [40,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 3000,
+        delay: function(el, i) {
+          return 500 + 30 * i;
+        }
+      }).add({
+        targets: '.ml12 .letter',
+        translateX: [0,-30],
+        opacity: [1,0],
+        easing: "easeInExpo",
+        duration: 2900,
+        delay: function(el, i) {
+          return 100 + 30 * i;
+        }
+      });
+    
+    // -=-=-=-=-=-=-=- This function creates the circle mail. -=-=-=-=-=-=-=-
+
+      anime.timeline({loop: true})
+      .add({
+        targets: '.ml8 .circle-white',
+        scale: [0, 3],
+        opacity: [1, 0],
+        easing: "easeInOutExpo",
+        rotateZ: 360,
+        duration: 1100
+      }).add({
+        targets: '.ml8 .circle-container',
+        scale: [0, 1],
+        duration: 1100,
+        easing: "easeInOutExpo",
+        offset: '-=1000'
+      }).add({
+        targets: '.ml8 .circle-dark',
+        scale: [0, 1],
+        duration: 1100,
+        easing: "easeOutExpo",
+        offset: '-=600'
+      }).add({
+        targets: '.ml8 .letters-left',
+        scale: [0, 1],
+        duration: 1200,
+        offset: '-=550'
+      }).add({
+        targets: '.ml8 .bang',
+        scale: [0, 1],
+        rotateZ: [45, 15],
+        duration: 1200,
+        offset: '-=1000'
+      }).add({
+        targets: '.ml8',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1400
+      });
+
+    anime({
+      targets: '.ml8 .circle-dark-dashed',
+      rotateZ: 360,
+      duration: 8000,
+      easing: "linear",
+      loop: true
+    });
+
+    
+  }
 
   
   render() {
     return (
       <div className="home">
+
         <Navi />
+
         <div className="home-content">
-        <div class='console-container'><span id='text'></span><div class='console-underscore' id='console'>&#95;</div></div>
+          
+          <h1 class="ml12">Hello my name is Joshua Matos Im a web developer from Miami</h1>
+          <div class="sk-folding-cube">
+            <div class="sk-cube1 sk-cube"></div>
+            <div class="sk-cube2 sk-cube"></div>
+            <div class="sk-cube4 sk-cube"></div>
+            <div class="sk-cube3 sk-cube"></div>
+          </div>
+
+          <h1 class="ml8">
+            <span class="letters-container">
+              <span class="letters letters-left"><img className="msgme" src="/images/whiteMailIcon.png" alt=""/></span>
+              <span class="letters bang">!</span>
+            </span>
+            <span class="circle circle-white"></span>
+            <span class="circle circle-dark"></span>
+            <span class="circle circle-container"><span class="circle circle-dark-dashed"></span></span>
+          </h1>
+
         </div>
+
       </div>
     )
   }
