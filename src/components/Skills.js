e11fad79ef683523f1ac373e9074ec1db84b624c
Josh Matos <jmatos.5@aol.com>
2019-04-09 16:42:32 -0400

    skills page started

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,10 +18,25 @@ class skills extends Component {
 
   render() {
     return (
+    
     <div className="skills-page">
-    
-      <h1 className="skills-header">Skills</h1>
-    
+
+      <div className="skillsContent">
+
+        <h1 className="skills-header">Skills & Experience</h1>
+        
+        <h2> I'm a <span className="white">programmer</span>.</h2>
+        
+      <p>
+        For the <span className="white">front-end</span> I usually work with Javascript, either standalone or including popular frameworks like ReactJS and VueJS. I also make the web pretty by using Sass, CSS and, whenever needed, any of their friends: Bootstrap, Bulma, etc.
+      </p>
+
+      <p>
+        For the <span className="white">back-end</span> I also work with Javascript (NodeJS, Express, MongoDB, etc). But, of course, whenever the project requires PHP, I do PHP as well (Wordpress, Laravel, etc).
+      </p>
+
+      </div>
+      
     </div>
     )
   }
