38ea9aa688d225b4dd4e8a166a20037122a01b1f
Josh Matos <jmatos.5@aol.com>
2019-04-18 14:24:51 -0400

    skills page finished

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -28,7 +28,7 @@ class skills extends Component {
         <h2> I'm a <span className="white">programmer</span>.</h2>
         
       <p>
-        For the <span className="white">front-end</span> I usually work with Javascript, either standalone or including popular frameworks like ReactJS and VueJS. I also make the web pretty by using Sass, CSS and, whenever needed, any of their friends: Bootstrap, Bulma, etc.
+        For the <span className="white">front-end</span> I usually work with Javascript, either standalone or including popular frameworks like ReactJS. I also make the web pretty by using Sass, CSS and, whenever needed, any of their friends: Bootstrap, Bulma, etc.
       </p>
 
       <p>
@@ -37,6 +37,76 @@ class skills extends Component {
 
       </div>
       
+      <section class="container">
+  
+  <h3>I'm a junior <span className="white">web developer</span> and here's what I can do</h3>
+  
+<div className="chartsec">
+
+
+  {/* <!-- HTML Chart --> */}
+  <div>
+  <figure class="chart" data-percent="75">
+    <figcaption>HTML</figcaption>
+     <img class="html" src="/images/htmlIcon.png"/>
+    <svg width="200" height="200">
+      <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+    </svg>
+  </figure>
+  <h3 className="white chartname htmlname">HTML</h3>
+  </div>
+  
+
+  {/* <!-- CSS Chart --> */}
+  <div>
+  <figure class="chart" data-percent="75">
+    <figcaption>CSS</figcaption>
+    <img class="css" src="/images/cssIcon.png"/>
+    <svg width="200" height="200">
+      <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+    </svg>
+  </figure>
+  <h3 className="white chartname cssname">CSS</h3>
+  </div>
+
+  {/* <!-- Javascript Chart --> */}
+  <div>
+  <figure class="chart" data-percent="50">
+    <figcaption>Javascript</figcaption>
+    <img class="javascript" src="/images/javascriptIcon.png"/>
+    <svg width="200" height="200">
+      <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+    </svg>
+  </figure>
+  <h3 className="white chartname jsname">JavaScript</h3>
+  </div>
+
+  {/* <!-- Node.js Chart --> */}
+  <div>
+  <figure class="chart" data-percent="25">
+    <figcaption>Node</figcaption>
+    <img class="node" src="/images/nodejs.png"/>
+    <svg width="200" height="200">
+      <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+    </svg>
+  </figure>
+  <h3 className="white chartname nodejsname">NodeJS</h3>
+  </div>
+
+  <div>
+    {/* <!-- ReactJS Chart --> */}
+    <figure class="chart" data-percent="50">
+    <figcaption>ReactJS</figcaption>
+    <img class="react" src="/images/reactIcon.png"/>
+    <svg width="200" height="200">
+      <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+    </svg>
+  </figure>
+  <h3 className="white chartname reactname">ReactJS</h3>
+  </div>
+</div>
+</section>
+
     </div>
     )
   }
